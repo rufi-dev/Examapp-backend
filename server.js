51b3d3de94fa2602bea22b7416fa458214fd8174
Rufi <68145072+Rufi-git@users.noreply.github.com>
2024-05-04 18:07:46 +0400

    cors

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(
     cors({
-        origin: ["https://riyaziyyat.vercel.app", "https://examonline.vercel.app", "http://localhost:5173"],
+        origin: ["https://sinaqriyaziyyat.vercel.app", "https://examonline.vercel.app", "http://localhost:5173"],
         credentials: true
     })
 )
