3a8e73036b1c5b3d489591fe1ab30562be955cbc
Rufi <68145072+Rufi-git@users.noreply.github.com>
2024-05-09 12:32:00 +0400

    dfssfd

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,66 +1,66 @@
-const asyncHandler = require("express-async-handler")
-const jwt = require("jsonwebtoken")
-const User = require("../models/userModel")
+const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
 
 const protect = asyncHandler(async (req, res, next) => {
-    try {
-        const token = req.cookies.token
+  try {
+    const token = req.cookies.token;
 
-        if (!token) {
-            res.status(401)
-            throw new Error('Not authorized, please login')
-        }
-
-        // Verify Token
-        const verified = jwt.verify(token, process.env.JWT_SECRET)
-
-        //Get UserId from Token
-        const user = await User.findById(verified.id).select("-password")
-
-        if (!user) {
-            res.status(404)
-            throw new Error('User not found')
-        }
-
-        if (user.role === 'suspended') {
-            res.status(400)
-            throw new Error('User suspended, please contact support')
-        }
-
-        req.user = user
-        next()
-    } catch (error) {
-        res.status(401)
-        throw new Error('Not authorized, please login')
+    if (!token) {
+      res.status(401);
+      throw new Error("Not authorized, please login");
     }
-})
 
+    // Verify Token
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+
+    //Get UserId from Token
+    const user = await User.findById(verified.id).select("-password");
+
+    if (!user) {
+      res.status(404);
+      throw new Error("User not found");
+    }
+
+    if (user.role === "suspended") {
+      res.status(400);
+      throw new Error("User suspended, please contact support");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401);
+    console.log("protect: ", error);
+    throw new Error(`Not authorized, please login: ${error}`);
+  }
+});
 
 const adminOnly = asyncHandler(async (req, res, next) => {
-    if (req.user && req.user.role === "admin") {
-        next()
-    } else {
-        res.status(401)
-        throw new Error('Not authorized as an admin')
-    }
-})
+  if (req.user && req.user.role === "admin") {
+    next();
+  } else {
+    res.status(401);
+    throw new Error("Not authorized as an admin");
+  }
+});
 
 const teacherOnly = asyncHandler(async (req, res, next) => {
-    if (req.user && (req.user.role === "teacher" || req.user.role === "admin")) {
-        next()
-    } else {
-        res.status(401)
-        throw new Error('Not authorized as teacher or admin')
-    }
-})
+  if (req.user && (req.user.role === "teacher" || req.user.role === "admin")) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error("Not authorized as teacher or admin");
+  }
+});
 
 const verifiedOnly = asyncHandler(async (req, res, next) => {
-    if (req.user && req.user.isVerified) {
-        next()
-    } else {
-        res.status(401)
-        throw new Error('Not authorized, account not verified!')
-    }
-})
+  if (req.user && req.user.isVerified) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error("Not authorized, account not verified!");
+  }
+});
 
-module.exports = { protect, adminOnly, teacherOnly, verifiedOnly }
+module.exports = { protect, adminOnly, teacherOnly, verifiedOnly };
